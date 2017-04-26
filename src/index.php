65a1da0d48a63dd88979e6584ec576491427423a
Eva Lai <evalai.dc@gmail.com>
2017-04-26 17:49:58 -0400

    added reminder notes

--- src/index.php
+++ src/index.php
@@ -1,4 +1,11 @@
+
+
 <?php
 echo "Hello, World!";
+// prints current version of PHP
 echo ' Current PHP version: ' . phpversion();
+
+// run below line in order to run app  from the src file to the docker volumes file
+  // $ docker run -p 80:80 -v /Users/elai/devops/src/:/var/www/html hello-world
+
 ?>
