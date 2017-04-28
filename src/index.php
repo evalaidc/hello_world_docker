5e7c32ee8a64a44e26f4feed9a7d0dceaa4c0517
Eva Lai <evalai.dc@gmail.com>
2017-04-28 13:09:20 -0400

    started ubuntu image and adjust nodejs shell

--- src/index.php
+++ src/index.php
@@ -7,5 +7,5 @@ echo ' Current PHP version: ' . phpversion();
 
 // run below line in order to run app  from the src file to the docker volumes file
   // $ docker run -p 80:80 -v /Users/elai/devops/src/:/var/www/html hello-world
-
+  // provides local copy to container version
 ?>
