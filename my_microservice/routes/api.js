de04fa3f4114320059f4324d6a0cb1617aff0f8d
Eva Lai <evalai.dc@gmail.com>
2017-04-28 13:32:01 -0400

    changed app.js file in microservice express app to pull from api

--- my_microservice/routes/api.js
+++ my_microservice/routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET greeting. */
 router.get('/sayhello', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Hello Audience!');
 });
 
 module.exports = router;
